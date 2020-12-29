2ae129e9eeeef1bc6d064efec6a0e94288d62205
Artem Safta <safta.artem@gmail.com>
2020-12-29 14:55:48 +0200

    change pixelSize to observable

--- view/frontend/web/js/viewmodel.js
+++ view/frontend/web/js/viewmodel.js
@@ -1,4 +1,11 @@
 define(['uiComponent', 'Safta_Module/js/ko-binding/pixelbg'], function (Component) {
     'use strict';
-    return Component.extend({});
+    return Component.extend({
+        defaults: {
+            pixelSize: 10,
+            tracks: {
+                pixelSize: true
+            }
+        }
+    });
 });
